91ca83ec74ac5fa4a1cbdbf7abf72b1dac08e21d
schantepie <stephane.chantepie@gmail.com>
2015-11-19 11:29:22 +0100

    new in2p3 codes

--- Codes_in2p3/multiple_batch/gmat.sh
+++ Codes_in2p3/multiple_batch/gmat.sh
@@ -15,5 +15,3 @@ for i in LHTMORPH_exp2 LHTMORPH_SI2 morph_exp morph_SI LHT_exp LHT_SI
     qsub  -l ct=46:00:00 -l vmem=2G -l fsize=50M -j y -o "${lieu1}_results"/qsout.txt $PWD/R2.sh ${i} ${j} $lieu1 $lieu2
     done
   done 
-  
-  
