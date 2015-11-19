6e8c8f9860c45eadd1b5b951c84f9a2b61357d63
schantepie <stephane.chantepie@gmail.com>
2015-11-19 11:53:30 +0100

    new batch codes in2p3

---
+++ Codes_in2p3/multiple_batch/README.md
@@ -0,0 +1,7 @@
+For using these codes with .R files
+
+1. upload a file on in2p3 with : data, pedigree and R scripts
+2. paste gmat.sh and R2.sh 
+3. line 6 of gmat.sh : add the name of your .R (note #)
+
+A folder will be created for each .R with respective results 
--- Codes_in2p3/multiple_batch/gmat.sh
+++ Codes_in2p3/multiple_batch/gmat.sh
@@ -15,3 +15,18 @@ for i in LHTMORPH_exp2 LHTMORPH_SI2 morph_exp morph_SI LHT_exp LHT_SI
     qsub  -l ct=46:00:00 -l vmem=2G -l fsize=50M -j y -o "${lieu1}_results"/qsout.txt $PWD/R2.sh ${i} ${j} $lieu1 $lieu2
     done
   done 
+  
+#   exemple if you want to run  a different number of jobs for somme .R 
+#   
+#   for i in  morph_SI LHT_exp LHT_SI # LHTMORPH_exp2
+#   do 
+#     
+#   mkdir "$PWD/${i}_results"
+#   lieu1="$PWD/${i}"
+#   lieu2="$PWD/"
+#   for j in `seq 5`
+#     do
+#     echo "iteration_${j}"
+#     qsub  -l ct=46:00:00 -l vmem=2G -l fsize=50M -j y -o "${lieu1}_results"/qsout.txt $PWD/R2.sh ${i} ${j} $lieu1 $lieu2
+#     done
+#   done 
